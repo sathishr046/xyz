0680e1befc3fd18af13cd41682f7e0ac971e090f
Srinivas712-P <Srinivas_712>
2025-07-31 22:21:12 +0530

    Initial commit

--- src/config/api.config.js
+++ src/config/api.config.js
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-    WEATHER_API_KEY: '2a7db0675d878da3acb95a9d341d3d4e', // OpenWeatherMap API key
+    WEATHER_API_KEY: '0f09309df2ee8f8a6d0628df5dcf104e', // OpenWeatherMap API key
     WEATHER_BASE_URL: 'https://api.openweathermap.org/data/2.5',
     // Add other API configurations here
   };
